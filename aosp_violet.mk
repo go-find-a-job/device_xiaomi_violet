1abc0b9aaea147a579db8303db20ea25a759121e
Shubham Singh <coolsks94@gmail.com>
2019-03-30 00:04:47 +0100

    Add support to build both gapps and non-gapps build

--- aosp_violet.mk
+++ aosp_violet.mk
@@ -34,7 +34,11 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+# Set this flag in build script
+ifeq ($(CURRENT_BUILD_TYPE), gapps)
 # Use Gapps
+TARGET_SHIPS_SEPERATE_GAPPS_BUILD := true
 WITH_GAPPS := true
 TARGET_GAPPS_ARCH := arm64
-IS_PHONE := true 
+IS_PHONE := true
+endif
